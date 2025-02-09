5a73f80dd87e9fd7518ed96c6f3c9ed71e52622c
Bruna Moura de Brito <bruunamoura@hotmail.com>
2025-02-09 16:15:53 -0300

    Integrated data info to html.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from data_processing import process_legislators
+from data_processing import process_legislators, process_bills
 
 app = Flask(__name__)
 
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     legislators_data = process_legislators()
-    return render_template("index.html", legislators=legislators_data)
+    bills_data = process_bills()
+    return render_template("index.html", legislators=legislators_data, bills=bills_data)
 
 
 if __name__ == "__main__":
