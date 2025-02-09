083585ee8c3b5289f7f26666a519f657f08de8f0
Bruna Moura de Brito <bruunamoura@hotmail.com>
2025-02-09 20:58:08 -0300

    Get more bill info through csv.

--- data_processing.py
+++ data_processing.py
@@ -20,15 +20,33 @@ def process_legislators():
 
 def process_bills():
     votes_merged = vote_results_df.merge(votes_df, left_on="vote_id", right_on="id")
+    votes_merged = votes_merged.merge(legislators_df, left_on="legislator_id", right_on="id", how="left")
+
     bill_votes = votes_merged.groupby("bill_id")["vote_type"].value_counts().unstack(fill_value=0)
+
+    supporters_dict = (
+        votes_merged[votes_merged["vote_type"] == 1]
+        .groupby("bill_id")["name"]
+        .apply(list)
+        .to_dict()
+    )
+
+    opposers_dict = (
+        votes_merged[votes_merged["vote_type"] == 2]
+        .groupby("bill_id")["name"]
+        .apply(list)
+        .to_dict()
+    )
 
     results = bills_df.copy()
     results["supporters"] = results["id"].map(bill_votes.get(1, 0))
     results["opposers"] = results["id"].map(bill_votes.get(2, 0))
+    results["supporter_names"] = results["id"].map(supporters_dict).apply(lambda x: x if isinstance(x, list) else [])
+    results["opposer_names"] = results["id"].map(opposers_dict).apply(lambda x: x if isinstance(x, list) else [])
 
     results = results.merge(legislators_df, left_on="sponsor_id", right_on="id", how="left")
-
     results["name"] = results["name"].fillna("Not registered")
-    response = results[["id_x", "title", "supporters", "opposers", "name"]].to_dict(orient="records")
+
+    response = results[["id_x", "title", "supporters", "opposers", "supporter_names", "opposer_names", "name"]].to_dict(orient="records")
 
     return response
