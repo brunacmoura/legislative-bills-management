f064ec43fb640dcaf457790e6c78aa06acb836b3
Bruna Moura de Brito <bruunamoura@hotmail.com>
2025-02-09 10:09:39 -0300

    Transformed all csv to dataframe to access data.

---
+++ data_processing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+bills_df = pd.read_csv("data/bills.csv")
+legislators_df = pd.read_csv("data/legislators.csv")
+votes_df = pd.read_csv("data/votes.csv")
+vote_results_df = pd.read_csv("data/vote_results.csv")
